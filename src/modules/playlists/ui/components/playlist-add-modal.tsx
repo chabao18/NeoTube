32162ba760fa4d35c5b9a302066a398b38120365
Zhixiong Lu <zhixiong.lu@qq.com>
2025-07-17 23:07:34 +0800

    feat: populating playlist

---
+++ src/modules/playlists/ui/components/playlist-add-modal.tsx
@@ -0,0 +1,112 @@
+import { InfiniteScroll } from "@/components/infinite-scroll";
+import { ResponsiveModal } from "@/components/responsive-dialog";
+import { Button } from "@/components/ui/button";
+import { DEFAULT_LIMIT } from "@/constants";
+import { trpc } from "@/trpc/client";
+import { Loader2Icon, SquareCheckIcon, SquareIcon } from "lucide-react";
+import { toast } from "sonner";
+
+interface PlaylistAddProps {
+  videoId: string;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const PlaylistAddModal = ({
+  videoId,
+  open,
+  onOpenChange,
+}: PlaylistAddProps) => {
+  const utils = trpc.useUtils();
+  const {
+    data: playlists,
+    isLoading,
+    hasNextPage,
+    isFetchingNextPage,
+    fetchNextPage,
+  } = trpc.playlist.getManyForVideo.useInfiniteQuery(
+    {
+      limit: DEFAULT_LIMIT,
+      videoId,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+      enabled: !!videoId && open,
+    }
+  );
+
+  const addVideo = trpc.playlist.addVideo.useMutation({
+    onSuccess: (data) => {
+      toast.success("Video added to playlist");
+      utils.playlist.getMany.invalidate();
+      utils.playlist.getManyForVideo.invalidate({ videoId });
+    },
+
+    onError: () => {
+      toast.error("Something went wrong");
+    },
+  });
+
+  const removeVideo = trpc.playlist.removeVideo.useMutation({
+    onSuccess: () => {
+      toast.success("Video removed from playlist");
+      utils.playlist.getMany.invalidate();
+      utils.playlist.getManyForVideo.invalidate({ videoId });
+    },
+
+    onError: () => {
+      toast.error("Something went wrong");
+    },
+  });
+
+  return (
+    <ResponsiveModal
+      title="Add to playlist"
+      open={open}
+      onOpenChange={onOpenChange}
+    >
+      <div className="flex fc gap-2">
+        {isLoading && (
+          <div className="flex justify-center p-4">
+            <Loader2Icon className="size-5 animate-spin to-muted-foreground" />
+          </div>
+        )}
+        {!isLoading &&
+          playlists?.pages
+            .flatMap((page) => page.items)
+            .map((playlist) => (
+              <Button
+                key={playlist.id}
+                variant="ghost"
+                className="w-full justify-start px-2 [&_svg]:size-5"
+                size="lg"
+                onClick={() => {
+                  if (playlist.containsVideo) {
+                    removeVideo.mutate({ playlistId: playlist.id, videoId });
+                  } else {
+                    addVideo.mutate({ playlistId: playlist.id, videoId });
+                  }
+                }}
+                disabled={removeVideo.isPending || addVideo.isPending}
+              >
+                {playlist.containsVideo ? (
+                  <SquareCheckIcon className="mr-2" />
+                ) : (
+                  <SquareIcon className="mr-2" />
+                )}
+                {playlist.name}
+              </Button>
+            ))}
+
+        {!isLoading && (
+          <InfiniteScroll
+            hasNextPage={hasNextPage}
+            isFetchingNextPage={isFetchingNextPage}
+            fetchNextPage={fetchNextPage}
+            isManual
+          />
+        )}
+      </div>
+    </ResponsiveModal>
+  );
+};
--- src/modules/videos/ui/components/video-menu.tsx
+++ src/modules/videos/ui/components/video-menu.tsx
@@ -6,12 +6,14 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { APP_URL } from "@/constants";
+import { PlaylistAddModal } from "@/modules/playlists/ui/components/playlist-add-modal";
 import {
   ListPlusIcon,
   MoreVerticalIcon,
   ShareIcon,
   Trash2Icon,
 } from "lucide-react";
+import { useState } from "react";
 import { toast } from "sonner";
 
 interface VideoMenuProps {
@@ -31,29 +33,38 @@ export const VideoMenu = ({
     toast.success("Link copied to the clipboard");
   };
 
+  const [openPlaylistAddModal, setOpenPlaylistAddModal] = useState(false);
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant={variant} size="icon" className="rounded-full">
-          <MoreVerticalIcon />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" onClick={(e) => e.stopPropagation()}>
-        <DropdownMenuItem onClick={onShare}>
-          <ShareIcon className="mr-2 size-4" />
-          Share
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => {}}>
-          <ListPlusIcon className="mr-2 size-4" />
-          Add to playlist
-        </DropdownMenuItem>
-        {onRemove && (
-          <DropdownMenuItem onClick={() => {}}>
-            <Trash2Icon className="mr-2 size-4" />
-            Remove
+    <>
+      <PlaylistAddModal
+        videoId={videoId}
+        open={openPlaylistAddModal}
+        onOpenChange={setOpenPlaylistAddModal}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant={variant} size="icon" className="rounded-full">
+            <MoreVerticalIcon />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" onClick={(e) => e.stopPropagation()}>
+          <DropdownMenuItem onClick={onShare}>
+            <ShareIcon className="mr-2 size-4" />
+            Share
           </DropdownMenuItem>
-        )}
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <DropdownMenuItem onClick={() => setOpenPlaylistAddModal(true)}>
+            <ListPlusIcon className="mr-2 size-4" />
+            Add to playlist
+          </DropdownMenuItem>
+          {onRemove && (
+            <DropdownMenuItem onClick={() => {}}>
+              <Trash2Icon className="mr-2 size-4" />
+              Remove
+            </DropdownMenuItem>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   );
 };
